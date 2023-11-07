91a9ae16180330e20bb7190479aa7aa0e26f2e06
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-11-07 13:35:23 +0600

    Create BidRequest component to display bid requests - Fetched bid request data from the server based on the client's email using Axios. - Designed the BidRequest component to display bid request information. - Integrated the Navbar component for consistent navigation.

--- src/pages/BidRequest/BidRequest.jsx
+++ src/pages/BidRequest/BidRequest.jsx
@@ -1,6 +1,25 @@
+import { useContext, useEffect, useState } from "react";
 import Navbar from "../../sharedComponents/Navbar/Navbar";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import axios from "axios";
 
 const BidRequest = () => {
+    const {user}=useContext(AuthContext)
+
+    const [bidRequest, setBidRequest] = useState([])
+
+    const url = `http://localhost:3000/api/v1/bid-request?clientEmail=${user?.email}`
+
+    useEffect(() => {
+        axios.get(url)
+            .then(res => {
+                console.log(res.data);
+                setBidRequest(res.data)
+            })
+    }, [url])
+
+    console.log(bidRequest);
+
     return (
         <div>
             {/* Navbar */}
