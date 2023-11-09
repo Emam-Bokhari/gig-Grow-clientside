8f9c3064e5f42d17c552a5f683a75646fe3dee0e
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-11-09 13:14:28 +0600

    Change MyBidsTable component pending button background color

--- src/pages/MyBids/MyBidsTable.jsx
+++ src/pages/MyBids/MyBidsTable.jsx
@@ -18,15 +18,15 @@ const MyBidsTable = ({ data, index }) => {
 
                     <div className="bg-[#f6f8fa] text-[#cad0d6] h-16 py-3">
                         {status === 'aceapt' ? (
-                            <button className=" px-2 md:px-4 py-1 rounded text-base md:text-lg text-green-500">
+                            <button className=" px-2 md:px-4 py-1 rounded text-base md:text-lg text-green-500 cursor-default">
                                 In Progress
                             </button>
                         ) : status === 'reject' ? (
-                            <button className=" px-2 md:px-4 py-1 rounded text-base md:text-lg text-red-500">
+                            <button className=" px-2 md:px-4 py-1 rounded text-base md:text-lg text-red-500 cursor-default">
                                 Canceled
                             </button>
                         ) : (
-                            <button className="bg-[#19cb98] px-2 md:px-4 py-1 rounded text-base md:text-lg text-white">
+                            <button className=" px-2 md:px-4 py-1 rounded text-base md:text-lg text-white cursor-default bg-orange-400">
                                 Pending
                             </button>
                         )}
